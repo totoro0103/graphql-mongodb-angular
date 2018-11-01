bbb68df1a7f4357f636affc3fecb224af2c610dc
Vadim Nicolai <nicolai.vadim@gmail.com>
2018-11-01 12:16:11 +0200

    Added data from different additional source.

--- server/src/modules/task/TaskType.js
+++ server/src/modules/task/TaskType.js
@@ -1,4 +1,6 @@
-import TaskModel from './TaskModel'
+import axios from 'axios';
+import R from 'ramda';
+import TaskModel from './TaskModel';
 
 export const typeDefs = `
   type Task {
@@ -7,10 +9,28 @@ export const typeDefs = `
   }
 `
 
+const renameKeys = R.curry((keysMap, obj) =>
+  R.reduce((acc, key) => R.assoc(keysMap[key] || key, obj[key], acc), {}, R.keys(obj))
+);
+
+const getIssues = async () => {
+  const { data: issuesRes } = await axios.get('https://api.github.com/repos/vadimnicolai/graphql-mongodb-angular/issues')
+  const issues = R.map(
+    R.pipe(
+      R.pickAll(['id', 'title']),
+      renameKeys({ title: 'name' }),
+      R.merge({ 'label': 'GitHub issue', 'editable': false })
+    )
+  )(issuesRes);
+  return issues;
+};
+
 export const resolvers = {
   getAllTasks: async () => {
     const tasks = await TaskModel.find();
-    return tasks;
+    const issues = await getIssues();
+
+    return [...tasks, ...issues];
   },
   task: (parent, arg) => {
     return { name: arg.name }
